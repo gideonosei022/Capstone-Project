36ad7d8536686280aad7f385e1fd1b98250cf332
Gideon Osei <gideonosei022@gmail.com>
2026-02-10 13:13:02 +0000

    Fix Django settings module path for Render

--- rental/rental/wsgi.py
+++ rental/rental/wsgi.py
@@ -11,6 +11,7 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'rental.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'rental.rental.settings')
+
 
 application = get_wsgi_application()
